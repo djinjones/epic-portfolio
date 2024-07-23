0028e33921e5cca6ae2b2b24cab2092fd9ba02a0
djinjones <160979104+djinjones@users.noreply.github.com>
2024-07-22 19:03:20 -0600

    moved image directories

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -7,42 +7,42 @@ function Portfolio() {
         link: "https://github.com/djinjones/Sudoku-in-Browser",
         description: "simple in browser sudoku I built outsite of class",
         status: "complete",
-        source: "./src/assets/sudoku.png",
+        source: "./assets/images/sudoku.png",
        },
        {
         title: "brewquest",
         link: "https://github.com/djinjones/Brewery-Tracker",
         description: "simple app to track breweries across the US based on location",
         status: "complete",
-        source: "./src/assets/brewquest.png",
+        source: "./assets/images/brewquest.png",
        },
        {
         title: "weather dashboard",
         link: "https://github.com/djinjones/Weather-Dashboard",
         description: "simple app to track the weather in areas specefied by the user",
         status: "complete",
-        source: "./src/assets/weather.png",
+        source: "./assets/images/weather.png",
        },
        {
         title: "svg logo generator",
         link: "https://github.com/djinjones/SVG-logo-maker",
         description: "simple app that runs in the terminal to create 3 letter word svg logos",
         status: "complete",
-        source: "./src/assets/svgstar.png",
+        source: "./assets/images/svgstar.png",
        },
        {
         title: "Readme Generator",
         link: "https://github.com/djinjones/Readme-Generator",
         description: "simple app that allows users to create readme files using the terminal",
         status: "complete",
-        source: "./src/assets/readmegenerator.png",
+        source: "./assets/images/readmegenerator.png",
        },
        {
         title: "Circular polyrythm",
         link: "https://github.com/djinjones/circular-polyrythm",
         description: "simple webstie that allows users to speed up and slow down a polyrythm",
         status: "inProgress",
-        source: "./circularpolyrythm.png"
+        source: "./assets/images/circularpolyrythm.png"
        }
 
     ];
